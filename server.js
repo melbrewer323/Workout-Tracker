e5b4857180baccc31e54e140bfc059a4c5184832
Melissa Brewer <melramirez727@gmail.com>
2021-09-09 17:53:00 -0500

    problems with server, uninstalled and reinstalled npm

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTracker", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+    useNewUrlParser: true, 
+    useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/views.js"));
+ app.use(require("./routes/api.js"));
+ app.use(require("./routes/views.js"));
+
+
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`Now listening ${PORT}!`);
 });
