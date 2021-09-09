2315087a1cc59275af80caf4bb195978f583a240
Melissa Brewer <melramirez727@gmail.com>
2021-09-09 18:13:34 -0500

    views route to display stats page

--- routes/views.js
+++ routes/views.js
@@ -9,3 +9,8 @@ router.get("/", (req,res) => {
 router.get("/exercise", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
+
+router.get("/stats", (req,res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
